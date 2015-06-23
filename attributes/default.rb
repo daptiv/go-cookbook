4114c0a8718db3da05244ce249d5e9286fcb657c
Shawn Weitzel <shawn.weitzel@changepoint.com>
2015-06-22 17:27:19 -0700

    allow non-default service account on windows, update cookbook version, update download_url to download.go.cd

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['go']['backup_path'] = ''
 default['go']['backup_retrieval_type'] = 'subversion'
 
-default['go']['agent']['auto_register']         = false
-default['go']['agent']['auto_register_key']     = 'default_auto_registration_key'
+default['go']['agent']['auto_register'] = false
+default['go']['agent']['auto_register_key'] = 'default_auto_registration_key'
 default['go']['agent']['auto_register_resources'] = []
 default['go']['agent']['auto_register_environments'] = []
 
@@ -11,12 +11,16 @@ default['go']['agent']['auto_register_environments'] = []
 default['go']['agent']['instance_count'] = node['cpu']['total']
 default['go']['agent']['server_search_query'] =
   "chef_environment:#{node.chef_environment} AND recipes:go\\:\\:server"
-  
 
-default['go']['version']                       = '14.3.0-1186'
+default['go']['version'] = '15.1.0-1863'
 
 unless platform?('windows')
-  default['go']['agent']['java_home']             = '/usr/bin/java'
+  default['go']['agent']['java_home'] = '/usr/bin/java'
 end
 
 default['go']['server']['install_path'] = 'C:\Program Files (x86)\Go Server'
+
+# The default windows agent runs as "localsystem", if overriding be sure the
+# "Logon As A Service" right has been granted to the non-default account.
+default['go']['agent_windows']['service_user'] = nil
+default['go']['agent_windows']['service_password'] = nil
